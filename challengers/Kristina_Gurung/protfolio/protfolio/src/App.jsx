020c6cf4c9fcb794d6e07118074dd60067d37873
Kristina Gurung <kristinagurung982@example.com>
2026-04-23 11:16:13 +0545

    hello

--- challengers/Kristina_Gurung/protfolio/protfolio/src/App.jsx
+++ challengers/Kristina_Gurung/protfolio/protfolio/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1 className="h-50 w-50 text-red-500 bg-blue-400 flex items-center justify-center">
-        Hello World!
-      </h1>
-    </div>
+    <>
+      <Navbar />
+      <main className="app-content">
+        <h1 className="h-50 w-50 text-red-500 bg-blue-400 flex items-center justify-center">
+          Hello World!
+        </h1>
+      </main>
+    </>
   )
 }
 
