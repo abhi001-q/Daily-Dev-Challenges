065f96a73e405bd5e68b9109bb1510741d2b6fb6
Kristina Gurung <kristinagurung982@example.com>
2026-04-23 11:23:57 +0545

    Clean up Kristina Gurung project structure and rename portfolio folder

--- challengers/Kristina_Gurung/portfolio/src/App.jsx
+++ challengers/Kristina_Gurung/portfolio/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
       <Navbar />
       <main className="app-content">
         <h1 className="h-50 w-50 text-red-500 bg-blue-400 flex items-center justify-center">
-          Hello World!
-        </h1>
+          Hello World!</h1>
+          
       </main>
     </>
   )
